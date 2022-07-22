501c80dea76bbf240c73c42a76464e92582a2fa9
Stanley Lo <stanleyjhl229@gmail.com>
2022-07-22 10:56:00 -0400

    difference in training-ready and googlesheets is because of anomalies

--- ml_for_opvs/data/preprocess/clean_donors_acceptors.py
+++ ml_for_opvs/data/preprocess/clean_donors_acceptors.py
@@ -789,8 +789,8 @@ class DAPairs:
 # # Step 3 - smiles_to_bigsmiles.py & smiles_to_selfies.py
 
 # # Step 4
-pairings = DAPairs(OPV_DATA, CLEAN_DONOR_CSV, CLEAN_ACCEPTOR_CSV)
-pairings.create_master_csv(MASTER_ML_DATA)
+# pairings = DAPairs(OPV_DATA, CLEAN_DONOR_CSV, CLEAN_ACCEPTOR_CSV)
+# pairings.create_master_csv(MASTER_ML_DATA)
 # pairings.create_master_csv(MASTER_ML_DATA_PLOT)
 
 # # # # Step 4b - Convert STR -> FLOAT
