cfa5df8f9662887903c9da527010afebfc6323a7
rileyhickman <riley.hickman13@gmail.com>
2023-06-26 21:27:25 -0400

    Completed comparisons of top performing models on the other two datasets

--- code_/training/train_compare_Saeki.py
+++ code_/training/train_compare_Saeki.py
@@ -79,22 +79,22 @@ def main_compare_Saeki(target_feats: list[str], hyperopt: bool = False) -> None:
     subspace = None
 
     for model in ["RF", "HGB", ]:
-        # ECFP
-        main_ecfp_and_numeric(dataset=opv_dataset,
-                              regressor_type=model,
-                              scalar_filter=filter,
-                              subspace_filter=subspace,
-                              target_features=target_feats,
-                              transform_type=transform_type,
-                              hyperparameter_optimization=hyperopt)
-        # # mordred
-        # main_mordred_and_numeric(dataset=opv_dataset,
-        #                          regressor_type=model,
-        #                          scalar_filter=filter,
-        #                          subspace_filter=subspace,
-        #                          target_features=target_feats,
-        #                          transform_type=transform_type,
-        #                          hyperparameter_optimization=hyperopt)
+        # # ECFP
+        # main_ecfp_and_numeric(dataset=opv_dataset,
+        #                       regressor_type=model,
+        #                       scalar_filter=filter,
+        #                       subspace_filter=subspace,
+        #                       target_features=target_feats,
+        #                       transform_type=transform_type,
+        #                       hyperparameter_optimization=hyperopt)
+        # mordred
+        main_mordred_and_numeric(dataset=opv_dataset,
+                                 regressor_type=model,
+                                 scalar_filter=filter,
+                                 subspace_filter=subspace,
+                                 target_features=target_feats,
+                                 transform_type=transform_type,
+                                 hyperparameter_optimization=hyperopt)
 
 
 if __name__ == "__main__":
